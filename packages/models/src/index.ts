a81660ac6d96cd711bff393a0c4d0899f72a5e8a
Eric Smaxwill <eric@smaxwill.com>
2024-05-07 08:01:19 -0500

    comments

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,5 +1,7 @@
-export * as user from "./statesman/user";
+// Dropship models
 export * as address from "./dropship/address";
 export * as shipment from "./dropship/shipment";
-
+// Lockbox models
 export * as key from "./lockbox/key";
+// Statesman models
+export * as user from "./statesman/user";
