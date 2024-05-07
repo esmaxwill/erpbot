0e8620d475e8117a3a4709f9ddca363f346b517a
Eric Smaxwill <eric@smaxwill.com>
2024-05-07 07:55:38 -0500

    change url paths for the sub applications

--- apps/sheriff/src/index.ts
+++ apps/sheriff/src/index.ts
@@ -1,28 +1,10 @@
 import { Hono } from "hono";
 import { Bindings } from "./bindings";
 
-import { keygen } from "@repo/lockbox";
 import * as auth from "./auth";
-
-declare module "hono" {
-  interface ContextVariableMap {}
-}
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.route("/auth", auth.default);
-
-app.use("*");
-
-app.get("/keygen", async (c) => {
-  return c.json(await keygen.generate());
-});
-
-app.get("/keygen/:seed", async (c) => {
-  const seed = c.req.param("seed");
-  return c.json(
-    await keygen.deterministic(c.env.KEY_INFO, new TextEncoder().encode(seed)),
-  );
-});
 
 export default app;
