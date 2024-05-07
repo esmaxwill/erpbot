bfd6e5c324c127a1fcd58f100b8efe25a6691a98
Eric Smaxwill <eric@smaxwill.com>
2024-05-07 07:49:15 -0500

    Add basic frontend app

--- apps/banker/src/index.ts
+++ apps/banker/src/index.ts
@@ -1,14 +1,14 @@
 import { Hono, Context } from "hono";
 import { Bindings } from "./bindings";
 
+import { CookieMiddlewareOpts, cookieMiddleware } from "@repo/cookie-auth";
+
 declare module "hono" {
   interface ContextVariableMap {}
 }
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.use("*", async (c: Context, next) => {
-  await next();
-});
+app.use("*", cookieMiddleware<{Bindings: Bindings}>({ cookieName: "session" }));
 
 export default app;
--- packages/cookie-auth/src/index.ts
+++ packages/cookie-auth/src/index.ts
@@ -4,11 +4,11 @@ import { HTTPException } from 'hono/http-exception';
 import { getCookie, getSignedCookie, setCookie, setSignedCookie, deleteCookie } from 'hono/cookie'
 import { createMiddleware } from "hono/factory";
 
-import type { Env } from "hono/types";
+import type { Env, Schema } from "hono/types";
 import type { CookieMiddlewareOpts } from "./types";
 
-export const cookieMiddleware = <T extends Env>(opts: CookieMiddlewareOpts) => {
-  return createMiddleware<T>(async (c: Context, next: Next) => {
+export const cookieMiddleware = <E extends Env>(opts: CookieMiddlewareOpts) => {
+  return createMiddleware<{Bindings: E}>(async (c: Context, next: Next) => {
     const secret = c.env.COOKIE_SECRET;
     if(!secret) {
         throw new HTTPException(500, {message: "Cookie secret is not set."});
