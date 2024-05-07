1d0560739373dd645d320f6f2bbbc7dc9dbc22fe
Eric Smaxwill <eric@smaxwill.com>
2024-05-07 07:59:24 -0500

    add lockbox models

---
+++ packages/models/src/lockbox/key.ts
@@ -0,0 +1,15 @@
+import {z} from 'zod';
+import { ulidFactory } from "ulid-workers";
+const ulid = ulidFactory();
+
+
+function generateId(): string {
+  return ulid();
+}
+
+export const KeySchema = z.object({
+  id: z.string().ulid().default(generateId)
+});
+
+
+export type Key = z.infer<typeof KeySchema>;
