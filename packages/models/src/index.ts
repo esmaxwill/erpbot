5053b126c11f05c591f9da4c2e680c4a48d811ec
Eric Smaxwill <eric@smaxwill.com>
2024-05-07 08:00:39 -0500

    add lockbox models

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,3 +1,5 @@
-export * as user from "./user";
+export * as user from "./statesman/user";
 export * as address from "./dropship/address";
 export * as shipment from "./dropship/shipment";
+
+export * as key from "./lockbox/key";
