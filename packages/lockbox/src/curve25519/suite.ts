4305af787571cb751bb3d34f8d0710458bdd0287
Eric Smaxwill <eric@smaxwill.com>
2024-05-07 08:28:45 -0500

    change aead to aes256gcm

--- packages/lockbox/src/curve25519/suite.ts
+++ packages/lockbox/src/curve25519/suite.ts
@@ -3,5 +3,5 @@ import { AeadId, CipherSuite, KdfId, KemId } from "hpke-js";
 export const suite = new CipherSuite({
   kem: KemId.DhkemX25519HkdfSha256,
   kdf: KdfId.HkdfSha256,
-  aead: AeadId.Aes128Gcm,
+  aead: AeadId.Aes256Gcm,
 });
